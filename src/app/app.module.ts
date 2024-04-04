b2887587e4b76757b4f04f290cb5c8c82986852a
priyankasnirgude <priyankaapatil2145@gmail.com>
2024-04-04 15:19:11 +0530

    work on directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './shared/components/product/product.component';
 import { ProductsComponent } from './shared/components/products/products.component';
 import { FormsModule } from '@angular/forms';
+import { PostsComponent } from './shared/components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    ProductsComponent
+    ProductsComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
